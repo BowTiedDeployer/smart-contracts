ed06df6fc9246718b3ed2e79cd745c818b87dc44
BowTiedDeployer <bowtieddeployer@gmail.com>
2022-08-01 17:50:03 +0300

    corrected generate jsons and images functions

--- customizable-nfts/backend/createJsonComponent.js
+++ customizable-nfts/backend/createJsonComponent.js
@@ -66,7 +66,7 @@ fs.readdir(pathCarImg, (err, cars) => {
       carName,
       `ipfs://QmY6NmxfHYRvW4q2jvkUbYqJto6boQG8RScwUkuie3gbjD/${car}`,
       [{ trait_type: 'Car', value: carName }],
-      'DegenBackgrounds'
+      'DegenCars'
     );
     fs.writeFile(pathCarGenerated + carName + '.json', metadata, function (err, result) {
       if (err) console.log('error', err);
@@ -85,7 +85,7 @@ fs.readdir(pathRimsImg, (err, rims) => {
       rimName,
       `ipfs://QmWBHgQFLKpXW8EnU6LeRXcU2iZRCpsSMCgfnb7G6obA31/${rim}`,
       [{ trait_type: 'Rims', value: rimName }],
-      'DegenBackgrounds'
+      'DegenRims'
     );
     fs.writeFile(pathRimsGenerated + rimName + '.json', metadata, function (err, result) {
       if (err) console.log('error', err);
@@ -97,10 +97,10 @@ fs.readdir(pathRimsImg, (err, rims) => {
 // extra step
 
 const pathHeadGenerated = './generated/jsons/Head/';
-const pathHeadNYC = '';
-const pathFaceNYC = '';
-const pathHeadMiami = '';
-const pathFaceMiami = '';
+const pathHeadNYCImg = prefix + 'AlienHead/';
+const pathFaceNYCImg = prefix + 'AlienFace/';
+const pathHeadMiami = prefix + 'SkullHead/';
+const pathFaceMiami = prefix + 'SkullFace/';
 
 // read every combination from nyc and create all possible heads in json format
 
@@ -108,22 +108,92 @@ const pathFaceMiami = '';
 
 // const pathHeadImg = prefix + 'Head/';
 
-// fs.readdir(pathHeadImg, (err, heads) => {
+// Goes through all NYC heads and faces
+fs.readdir(pathHeadNYCImg, (err, heads) => {
+  heads.forEach((head) => {
+    const headName = head.split('.')[0];
+    fs.readdir(pathFaceNYCImg, (err, faces) => {
+      faces.forEach((face) => {
+        const faceName = face.split('.')[0];
+
+        const metadata = composeMetadata(
+          'NYC_' + headName + '_' + faceName,
+          `ipfs://QmeBHNDwNUvwDR9rDT4Vh3oXCxsLyMSLwV2oPk8uAdQpxK/NYC_${headName}_${faceName}.png`,
+          [
+            { trait_type: 'Race', value: 'Alien' },
+            { trait_type: 'Head', value: headName },
+            { trait_type: 'Face', value: faceName },
+          ],
+          'DegenHeads'
+        );
+        fs.writeFile(
+          pathHeadGenerated + 'NYC_' + headName + '_' + faceName + '.json',
+          metadata,
+          function (err, result) {
+            if (err) console.log('error', err);
+          }
+        );
+      });
+    });
+  });
+});
+
+fs.readdir(pathHeadMiami, (err, heads) => {
+  heads.forEach((head) => {
+    const headName = head.split('.')[0];
+    fs.readdir(pathFaceMiami, (err, faces) => {
+      faces.forEach((face) => {
+        const faceName = face.split('.')[0];
+
+        const metadata = composeMetadata(
+          'Miami_' + headName + '_' + faceName,
+          `ipfs://QmeBHNDwNUvwDR9rDT4Vh3oXCxsLyMSLwV2oPk8uAdQpxK/Miami_${headName}_${faceName}.png`,
+          [
+            { trait_type: 'Race', value: 'Skull' },
+            { trait_type: 'Head', value: headName },
+            { trait_type: 'Face', value: faceName },
+          ],
+          'DegenHeads'
+        );
+        fs.writeFile(
+          pathHeadGenerated + 'Miami_' + headName + '_' + faceName + '.json',
+          metadata,
+          function (err, result) {
+            if (err) console.log('error', err);
+          }
+        );
+      });
+    });
+  });
+});
+
+// fs.readdir(skullHead, (err, heads) => {
 //   heads.forEach((head) => {
 //     const headName = head.split('.')[0];
-
-//     const metadata = composeMetadata(
-//       headName,
-//       `ipfs://QmWBHgQFLKpXW8EnU6LeRXcU2iZRCpsSMCgfnb7G6obA31/${head}`,
-//       [
-//         { trait_type: 'Race', value: 'Skull/Alien' },
-//         { trait_type: 'Head', value: headName },
-//         { trait_type: 'Face', value: 'faceName' },
-//       ],
-//       'DegenBackgrounds'
-//     );
-//     fs.writeFile(pathHeadGenerated + headName + '.json', metadata, function (err, result) {
-//       if (err) console.log('error', err);
+//     fs.readdir(skullFace, (err, faces) => {
+//       faces.forEach((face) => {
+//         const faceName = face.split('.')[0];
+//         generateHead('Miami', head, face, headName, faceName);
+//       });
 //     });
 //   });
 // });
+
+// async function generateHead(city, head, face, headName, faceName) {
+//   const headPath = city == 'NYC' ? alienHead + head : city == 'Miami' ? skullHead + head : '';
+//   const facePath = city == 'NYC' ? alienFace + face : city == 'Miami' ? skullFace + face : '';
+//   const race = city == 'NYC' ? prefix + 'Alien.png' : city == 'Miami' ? prefix + 'Skull.png' : '';
+
+//   const img = await mergeImages([race, headPath, facePath], {
+//     Canvas: Canvas,
+//     Image: Image,
+//   }).then((b64) => {
+//     return b64;
+//   });
+
+//   const data = img.replace(/^data:image\/\w+;base64,/, '');
+//   const buf = Buffer.from(data, 'base64');
+//   fs.writeFile(pathHeadsGen + city + '_' + headName + '_' + faceName + '.png', buf, function (err, result) {
+//     if (err) console.log('error', err);
+//   });
+// }
