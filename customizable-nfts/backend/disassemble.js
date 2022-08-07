a47cd2543cb1b2cdf08c5d53d741f1337359a90e
BowTiedDeployer <bowtieddeployer@gmail.com>
2022-08-07 18:48:08 +0300

    disassembleFinalize backend SC call in progress

--- customizable-nfts/backend/disassemble.js
+++ customizable-nfts/backend/disassemble.js
@@ -7,9 +7,49 @@
 // (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.upgrade-contract disassemble-finalize u1 'STNHKEPYEPJ8ET55ZZ0M5A34J0R3N5FM2CMMMAZ6 "DarkPurple" "BentleyBlack" "ClassyCream" "Miami_Syringe_Cigar")
 // (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.body-kits get-token-uri u1)
 // (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.body-kits get-owner u1)
+import { standardPrincipalCV, cvToHex, hexToCV, cvToJSON } from '@stacks/transactions';
+import { StacksMocknet, StacksTestnet, StacksMainnet } from '@stacks/network';
+import { network, coreApiUrl, urlApis } from './consts.js';
+import { intToHexString } from './helper.js';
+import { getAttributesMapTraitValue } from './merge_helper_functions.js';
 
-// for the given id return this json from SC
-readOnlyFromSC(
+let networkN =
+  network === 'mainnet' ? new StacksMocknet() : network === 'testnet' ? new StacksTestnet() : new StacksMocknet();
+
+// for the given id return the read only json url
+
+const getTokenUriNFT = async (userAddress, contractAddress, contractName, functionName, idArg) => {
+  let address = userAddress;
+  let id = '010000000000000000' + intToHexString(idArg);
+  try {
+    address = cvToHex(standardPrincipalCV(address));
+    const url = `${coreApiUrl[network]}${urlApis.readOnly(contractAddress, contractName, functionName)}`;
+    let tokenUriNFT = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        sender: userAddress, // todo: check this
+        network: networkN,
+        arguments: [id],
+      }),
+    })
+      .then((res) => res.json())
+      .then((res2) => cvToJSON(hexToCV(res2.result)))
+      .then((res3) => res3.value.value.value);
+    return tokenUriNFT;
+  } catch (error) {
+    console.log(error.message, '| invalid address');
+  }
+};
+
+// feth json from url
+
+const fetchJsonUrl = async (url) => {
+  // todo: see if possible without async promise
+  return fetch(url).then((res) => res.json());
+};
+
+const urlNFT = await getTokenUriNFT(
   'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5',
   'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
   'degens',
@@ -17,6 +57,80 @@ readOnlyFromSC(
   1
 );
 
-// QmbX7UCSFLBvJa2yB4YxqZxhacrxiKUGbE6fHbQuYMhNhf
+console.log(urlNFT);
+
+const jsonFetched = await fetchJsonUrl(urlNFT);
+console.log(jsonFetched);
 
 // fetch url from this - get components from attributes
+console.log(getAttributesMapTraitValue(jsonFetched));
+
+// calls SC function to mint-name
+
+export async function chainMintNameDisassemble(address, url, nonce) {
+  try {
+    await mintNameUrl(address, url, nonce);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function mintNameUrl(address, url, nonce) {
+  try {
+    // principalCV
+    // stringCV
+    //nonce 
+    let txOptions = { // todo: finish mintNameUrl call
+
+    // publish-result-many (run-result (list 50 { lobby-id: uint, run-id: uint, address: principal, score: uint, rank: uint, rank-factor: uint, rewards: uint, rac: uint}))
+    let tupleArray = [];
+    for (let index = 0; index < resultsFromDB.length; index++) {
+      const obj = resultsFromDB[index];
+      console.log(obj);
+
+      const tupCV = tupleCV({
+        'lobby-id': uintCV(obj['lobby-id']),
+        // 'run-id': uintCV(obj['run-id']),
+        address: standardPrincipalCV(obj.address),
+        score: uintCV(obj.score),
+        rank: uintCV(obj.rank),
+        'sum-rank-factor': uintCV(0),
+        'rank-factor': uintCV(obj['rank-factor']),
+        rewards: uintCV(obj.rewards),
+        rac: uintCV(obj.rac),
+        nft: stringAsciiCV(obj.nft),
+      });
+      tupleArray.push(tupCV);
+    }
+    console.log('publishResults tupleArray ', tupleArray);
+    const l = listCV(tupleArray);
+    const functionArgs = [l];
+
+    const latestNonce = await getAccountNonce(wallets.owner.wallet);
+
+    let txOptions = {
+      contractAddress: contractAddress.split('.')[0],
+      contractName: contractAddress.split('.')[1],
+      functionName: 'publish-result-many',
+      functionArgs: functionArgs,
+      senderKey: wallets.owner.private_key,
+      network: network,
+      // postConditions,
+      postConditionMode: PostConditionMode.Allow,
+      fee: new BigNum(100000),
+      nonce: new BigNum(latestNonce),
+    };
+    console.log(wallets.owner);
+    // calculate fee
+    let transaction = await makeContractCall(txOptions);
+    const normalizedFee = await getNormalizedFee(transaction);
+
+    // set fee
+    txOptions.fee = new BigNum(normalizedFee);
+    transaction = await makeContractCall(txOptions);
+
+    const tx = await broadcastTransaction(transaction, network);
+    console.log('publishResults broadcasted tx: ', tx.txid);
+  } catch (error) {
+    console.log('publishResults error: ', error);
+  }
--- customizable-nfts/backend/helper.js
+++ customizable-nfts/backend/helper.js
@@ -1,16 +1,20 @@
-import { standardPrincipalCV, cvToHex, intToHexString } from '@stacks/transactions';
+import { standardPrincipalCV, cvToHex } from '@stacks/transactions';
 import { StacksMocknet, StacksTestnet, StacksMainnet } from '@stacks/network';
 import { network, coreApiUrl, urlApis } from './consts.js';
 let networkN =
   network === 'mainnet' ? new StacksMocknet() : network === 'testnet' ? new StacksTestnet() : new StacksMocknet();
 
+export const intToHexString = (number) => {
+  return number.toString(16).padStart(8 * 2, '0');
+};
+
 // todo: invalid json response body at http://localhost:3999/v2/contracts/call-read/ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM/degens/get-token-uri reason: Unexpected end of JSON input | invalid address
-export async function readOnlyFromSC(userAddress, contractAddress, contractName, functionName, args) {
+export async function readOnlyFromSC(userAddress, contractAddress, contractName, functionName, idArg) {
   // https://stacks-node-api.mainnet.stacks.co/v2/contracts/call-read/SP1SCEXE6PMGPAC6B4N5P2MDKX8V4GF9QDE1FNNGJ/nyc-degens/get-owner
   // https://stacks-node-api.mainnet.stacks.co/v2/contracts/call-read/{contract_address}/{contract_name}/{function_name}
 
   let address = userAddress;
-  let id = '010000000000000000' + intToHexString(args); //args.toString(16);
+  let id = '010000000000000000' + intToHexString(idArg); //idArg.toString(16);
   console.log('id', id);
   try {
     console.log(address);
@@ -22,20 +26,16 @@ export async function readOnlyFromSC(userAddress, contractAddress, contractName,
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        sender: address, // todo: check this
+        sender: userAddress, // todo: check this
         network: networkN,
         arguments: [id],
       }),
     });
     const data = await res;
-    console.log(data);
+    console.log(await data.json());
   } catch (error) {
     console.log(error.message, '| invalid address');
   }
-}
-
-function convertArgumentsForSCReadOnlyCall(args) {
-  return args;
 }
 
 // "@stacks/transactions": "^3.3.0",
@@ -50,45 +50,11 @@ function convertArgumentsForSCReadOnlyCall(args) {
 //   1
 // );
 
-readOnlyFromSC(
-  'SP1SCEXE6PMGPAC6B4N5P2MDKX8V4GF9QDE1FNNGJ',
-  'SP1SCEXE6PMGPAC6B4N5P2MDKX8V4GF9QDE1FNNGJ',
-  'nyc-degens',
-  'get-token-uri',
-  1
-);
-
-async function chainJoinUserLobbyConfirmed(idLobby, userAddress) {
-  let id = idLobby;
-  id = '010000000000000000' + intToHexString(id);
-  let address = userAddress;
-  try {
-    address = cvToHex(standardPrincipalCV(address));
-    const contractFunction = 'get-score';
-    const url = `${coreApiUrl}/v2/contracts/call-read/${contractAddress.split('.')[0]}/${
-      contractAddress.split('.')[1]
-    }/${contractFunction}`;
-
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        sender: wallets.owner.wallet,
-        network: network,
-        arguments: [id, address],
-      }),
-    });
-    const data = await res.json();
-    if (data.okay !== undefined && data.okay === false) {
-      return false;
-    } else if (data.okay === true) {
-      // console.log(clarity.hexToValue(data.result))
-      return true;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.log(error.message, '| invalid address');
-    return false;
-  }
-}
+// mainnet
+// readOnlyFromSC(
+//   'SP1SCEXE6PMGPAC6B4N5P2MDKX8V4GF9QDE1FNNGJ',
+//   'SP1SCEXE6PMGPAC6B4N5P2MDKX8V4GF9QDE1FNNGJ',
+//   'nyc-degens',
+//   'get-token-uri',
+//   1
+// );
