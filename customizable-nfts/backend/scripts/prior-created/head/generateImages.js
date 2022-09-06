d113b9433748c0662230e5b557f9a155c9dd159e
BowTiedDeployer <bowtieddeployer@gmail.com>
2022-09-06 03:42:14 +0300

    added head image generator

---
+++ customizable-nfts/backend/scripts/prior-created/head/generateImages.js
@@ -0,0 +1,74 @@
+import mergeImages from 'merge-images';
+import { Canvas, Image } from 'canvas';
+import fs from 'fs';
+
+const prefix = './';
+const pathHeadsGen = './generated/images/';
+const alienFace = './AlienFace/';
+const alienHead = './AlienHead/';
+const skullFace = './SkullFace/';
+const skullHead = './SkullHead/';
+const pathHeadsMapSC = './generated/headsMap.txt';
+
+// empty map SC before appending to it
+fs.writeFile(pathHeadsMapSC, '', function (err, result) {
+  if (err) console.log('error', err);
+});
+
+// Generate all NYC Heads
+async function generateHead(city, head, face, headName, faceName) {
+  const headPath = city == 'NYC' ? alienHead + head : city == 'Miami' ? skullHead + head : '';
+  const facePath = city == 'NYC' ? alienFace + face : city == 'Miami' ? skullFace + face : '';
+  const race = city == 'NYC' ? prefix + 'Alien.png' : city == 'Miami' ? prefix + 'Skull.png' : '';
+
+  const img = await mergeImages([race, headPath, facePath], {
+    Canvas: Canvas,
+    Image: Image,
+  }).then((b64) => {
+    return b64;
+  });
+
+  const data = img.replace(/^data:image\/\w+;base64,/, '');
+  const buf = Buffer.from(data, 'base64');
+  fs.writeFile(pathHeadsGen + city + '_' + headName + '_' + faceName + '.png', buf, function (err, result) {
+    if (err) console.log('error', err);
+  });
+}
+
+function convertCityHeadFaceToSCLine(city, head, face) {
+  return `(map-set name-url {name: "${city}_${head}_${face}"} {url: "ipfs://QmY549kDdJDSR89yWLF3PrkoPjs1ZWboRMkaqmVmTVgsH7/${city}_${head}_${face}.json"})\n`;
+}
+
+function writeNewLine(newLine) {
+  fs.appendFile(pathHeadsMapSC, newLine, function (err, result) {
+    if (err) console.log('error', err);
+  });
+}
+
+// Goes through all NYC heads and faces
+fs.readdir(alienHead, (err, heads) => {
+  heads.forEach((head) => {
+    const headName = head.split('.')[0];
+    fs.readdir(alienFace, (err, faces) => {
+      faces.forEach((face) => {
+        const faceName = face.split('.')[0];
+        generateHead('NYC', head, face, headName, faceName);
+        writeNewLine(convertCityHeadFaceToSCLine('NYC', headName, faceName));
+      });
+    });
+  });
+});
+
+// Goes through all Miami heads and faces
+fs.readdir(skullHead, (err, heads) => {
+  heads.forEach((head) => {
+    const headName = head.split('.')[0];
+    fs.readdir(skullFace, (err, faces) => {
+      faces.forEach((face) => {
+        const faceName = face.split('.')[0];
+        generateHead('Miami', head, face, headName, faceName);
+        writeNewLine(convertCityHeadFaceToSCLine('Miami', headName, faceName));
+      });
+    });
+  });
+});
