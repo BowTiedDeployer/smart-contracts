353f2341d1508f6e82f8986f4b75b8d7437c4339
BowTiedDeployer <bowtieddeployer@gmail.com>
2022-11-14 00:37:02 +0200

    fixed app.get test

--- bns-collection/server/app.js
+++ bns-collection/server/app.js
@@ -5,38 +5,33 @@ import path from 'path';
 
 const PORT = 5000;
 
-
-app.get('/test', (req, res)=>{
-  res.json({
-    "test: Hello world!"
-  })
-})
+app.get('/test', (req, res) => {
+  res.send('test: Hello world!');
+});
 
 app.get('/jsons/:name', (req, res) => {
   const json_name = req.params.name;
-  fs.readFile(`./../files_stored/jsons/${json_name}`, (err, data)=>{
-    if (err) res.status(500).send(err)
+  fs.readFile(`./../files_stored/jsons/${json_name}`, (err, data) => {
+    if (err) res.status(500).send(err);
     else res.send(JSON.parse(data));
   });
 });
 
 app.get('/images/:name', (req, res) => {
   const image_name = req.params.name;
-  fs.readFile(`./../files_stored/images/${image_name}`, (err, data)=>{
-    if (err) res.status(500).send(err)
+  fs.readFile(`./../files_stored/images/${image_name}`, (err, data) => {
+    if (err) res.status(500).send(err);
     else res.send(JSON.parse(data));
   });
 });
 
 app.get('/in-game/:name', (req, res) => {
   const image_name = req.params.name;
-  fs.readFile(`./../files_stored/in-game/${image_name}`, (err, data)=>{
-    if (err) res.status(500).send(err)
+  fs.readFile(`./../files_stored/in-game/${image_name}`, (err, data) => {
+    if (err) res.status(500).send(err);
     else res.send(JSON.parse(data));
   });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
