90cd66e40137ce1c90d7e8132c178ca7013157f5
BowTiedDeployer <bowtieddeployer@gmail.com>
2023-03-10 02:39:06 +0200

    BTC Degens: rarity score created for each NFT

--- bns-collection/server/json-attribute-counter.js
+++ bns-collection/server/json-attribute-counter.js
@@ -13,6 +13,10 @@ const mapAttributes = {
   head: {},
 };
 
+// each NFT has a rarity
+// place them all in an array/map
+const rarity = [0];
+
 const worthBuying = [];
 
 // const listBackgrounds=[]
@@ -39,6 +43,7 @@ fs.readdir(dir, (err, files) => {
     // Parse the JSON data
     const jsonData = JSON.parse(fileContents);
     const jsonAttributes = jsonData.attributes;
+
     // Iterate over the attributes array and search for the field with the specified name and value
     if (mapAttributes['backgrounds'][jsonAttributes[0].value]) mapAttributes['backgrounds'][jsonAttributes[0].value]++;
     else mapAttributes['backgrounds'][jsonAttributes[0].value] = 1;
@@ -69,58 +74,43 @@ fs.readdir(dir, (err, files) => {
     )
       worthBuying.push(jsonData.name.split('#')[1]);
   });
+
   // console.log(mapAttributes);
 
-  console.log(worthBuying.filter((item) => item >= 0 && item <= 100));
-  // {
-  //   backgrounds: { 'Lux Yellow': 337, 'Pine Green': 357, 'Deep Magenta': 306 },
-  //   cars: {
-  //     Black: 245,
-  //     Green: 159,
-  //     Brown: 145,
-  //     Gold: 60,
-  //     Sand: 288,
-  //     'Blue-Light': 103
-  //   },
-  //   rims: {
-  //     WSand: 1,
-  //     'Monkey-Brown': 714,
-  //     'Monkey-Dark': 227,
-  //     'Monkey-Pink': 57,
-  //     WBlack: 1
-  //   },
-  //   type: {
-  //     'Monkey-Brown': 1,
-  //     'Empty-Face': 280,
-  //     Teeth: 122,
-  //     Pipe: 61,
-  //     Smile: 126,
-  //     Pierce: 153,
-  //     Uuu: 121,
-  //     Surprized: 88,
-  //     'Cool-Glasses': 47,
-  //     'Monkey-Dark': 1
-  //   },
-  //   head: {
-  //     Horns: 1,
-  //     WBlack: 251,
-  //     WGrey: 158,
-  //     WAlloy: 117,
-  //     WGold: 58,
-  //     WSand: 265,
-  //     WGreen: 149,
-  //     Headphones: 1
-  //   },
-  //   face: {
-  //     Pierce: 1,
-  //     Headphones: 168,
-  //     'Empty-Head': 272,
-  //     Helmet: 126,
-  //     'Team-Cap': 174,
-  //     Horns: 129,
-  //     'Exposed-Brain': 76,
-  //     Crown: 53,
-  //     Smile: 1
-  //   }
-  // }
+  // console.log(worthBuying.filter((item) => item >= 0 && item <= 100));
+});
+
+// Read each file in the directory
+fs.readdir(dir, (err, files) => {
+  if (err) {
+    throw err;
+  }
+
+  // Iterate over each file
+  files.forEach((file) => {
+    // Construct the full path to the file
+    const filePath = path.join(dir, file);
+
+    // Read the contents of the file
+    const fileContents = fs.readFileSync(filePath);
+
+    // Parse the JSON data
+    const jsonData = JSON.parse(fileContents);
+    const jsonAttributes = jsonData.attributes;
+
+    let rarityNTF =
+      mapAttributes['backgrounds'][jsonAttributes[0].value] *
+      mapAttributes['cars'][jsonAttributes[1].value] *
+      mapAttributes['type'][jsonAttributes[2].value] *
+      mapAttributes['face'][jsonAttributes[3].value] *
+      mapAttributes['rims'][jsonAttributes[4].value] *
+      mapAttributes['head'][jsonAttributes[5].value];
+    // console log each mapAttribute from above
+
+    rarity.push(rarityNTF);
+  });
+
+  for (let i = 0; i < rarity.length; i++) {
+    console.log(rarity[i]);
+  }
 });
